f76851e499e4f7116d10bd62037dcf6d63b88568
Mike Foley <roofmanmike@gmail.com>
2022-05-27 08:33:03 -0400

    about

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $('body').css("background-color", "darkGrey");
 function getTime(){
 const d = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})
-
 $('#rmm1').text(d);
 }
 getTime();
