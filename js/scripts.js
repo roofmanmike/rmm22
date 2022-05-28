7cd8efea57557455dfe99612be5fa985cef25ceb
Mike Foley <roofmanmike@gmail.com>
2022-05-28 15:41:26 -0400

    na

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,42 @@ $('#rmm1').text(dateMessage);
 // $('#rmm2').text(d2);
 }
 getTime();
+var tpo1 = $('#tpo1');
+var qmess = document.getElementById("quoteMessage");
+var qmess2 = document.getElementById("quoteMessage2");
+
 $("#btnStart").click(function(){
-  alert("The paragraph was clicked.");
+  qmess.innerHTML = "Water Shed";
+  qmess2.innerHTML = "Gutter or Drain as WIDTH";
+  tpo1.hide();
 });
+// --------------------
+function printStuff(){
+  const xx = $('#getRunoff').val();
+  passItem(xx);
+}
+function passItem(item){
+  console.log(item);
+}
+$("#btnThat").click(function(){
+  const n = $('#getRunoff').val();
+area();
+});
+function area(){
+  const width = $('#getRunoff').val();
+  const length = $('#getSlope').val();
+  sum = mult(width, length);
+  alert (sum);
+}
+// -----------------
+function add(a,b){
+let sum = Number(a) + Number(b);
+  return sum;
+}
+function sub(a,b){
+let sum = Number(a) - Number(b);
+  return sum;
+}function mult(a,b){
+let sum = Number(a) * Number(b);
+  return sum;
+}
