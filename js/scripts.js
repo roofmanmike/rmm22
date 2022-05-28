3db9a0f27bfb96be4ebb1c45b41e8ad165fe90fd
Mike Foley <roofmanmike@gmail.com>
2022-05-27 23:11:27 -0400

    na

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $('body').css("background-color", "darkGrey");
 function getTime(){
-const d = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})
-$('#rmm1').text(d);
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 2).padStart(2, '0');
+var yyyy = today.getFullYear();
+var inOneMonth = mm + '/' + dd + '/' + yyyy;
+var dateMessage = "Generate a free online quote valid until " + inOneMonth;
+$('#rmm1').text(dateMessage);
+// $('#rmm2').text(d2);
 }
 getTime();
+$("#btnStart").click(function(){
+  alert("The paragraph was clicked.");
+});
