fbad6f6f58af28e7d282565df5f933f43d3b45a8
roofmanmike <62150260+roofmanmike@users.noreply.github.com>
2022-06-14 14:07:39 -0400

    na

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-$('body').css("background-color", "darkGrey");
 function getTime(){
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 2).padStart(2, '0');
-var yyyy = today.getFullYear();
-var inOneMonth = mm + '/' + dd + '/' + yyyy;
-var dateMessage = "Generate a free online quote valid until " + inOneMonth;
-$('#rmm1').text(dateMessage);
-// $('#rmm2').text(d2);
-}
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 2).padStart(2, '0');
+  var yyyy = today.getFullYear();
+  var inOneMonth = mm + '/' + dd + '/' + yyyy;
+  var dateMessage = "Generate a free online quote valid until " + inOneMonth;
+  $('#rmm1').text(dateMessage);
+  // $('#rmm2').text(d2);
+  }
+$('body').css("background-color", "darkGrey");
 getTime();
 var tpo1 = $('#tpo1');
 var quote2 = $('#quote2');
